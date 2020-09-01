9e80608d96c72882657e23b97ce26576145bbf07
Abhinandan18001 <abhinandan18001@iiitd.ac.in>
2020-09-01 18:44:19 +0530

    Updates

---
+++ precache-manifest.7c6096fea1106511cd76a09ae5c58bc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db90c620c1d0d51e283a16f902073403",
+    "url": "/Flash-Cards-React/index.html"
+  },
+  {
+    "revision": "8789e63b354b77d41982",
+    "url": "/Flash-Cards-React/static/css/main.090afa74.chunk.css"
+  },
+  {
+    "revision": "dd4b504c7df09e0c3ed5",
+    "url": "/Flash-Cards-React/static/js/2.96b0bbe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Flash-Cards-React/static/js/2.96b0bbe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8789e63b354b77d41982",
+    "url": "/Flash-Cards-React/static/js/main.9d4784bf.chunk.js"
+  },
+  {
+    "revision": "0ae4418d62b4cd8eeb5a",
+    "url": "/Flash-Cards-React/static/js/runtime-main.0b5e53ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Flash-Cards-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
